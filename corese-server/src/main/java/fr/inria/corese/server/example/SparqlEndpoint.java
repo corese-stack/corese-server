2064136b7424c80ca027218d5e646b0198596dbd
Erwan Demairy <erwan.demairy@inria.fr>
2018-05-17 10:44:03 +0200

    Resolve the javax.annotation.* no found problem.

--- corese-server/src/main/java/fr/inria/corese/server/example/SparqlEndpoint.java
+++ corese-server/src/main/java/fr/inria/corese/server/example/SparqlEndpoint.java
@@ -1,3 +1,5 @@
+package fr.inria.corese.server.example;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
